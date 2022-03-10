0b67f3e89fe8ec3292ba5731cbfe096c1a88ffc1
Sean <seanthewonderful@gmail.com>
2022-03-10 16:57:40 -0700

    commit

--- starter.py
+++ starter.py
@@ -40,42 +40,74 @@ numbers = [1,2,3,4,5,6,7,8,9,10]
 
 # Problem 8
 # Use a for-in loop to iterate over the `numbers` array and print each number.
-
+for number in numbers:
+    print(number)
 
 # Problem 9
 # Create an empty array called `even_numbers`.
-# Use a for-in loop to iterate over the `numbers` array, and if a number is even, add  it to the `even_numbers` array.
-
+# Use a for-in loop to iterate over the `numbers` array, and if a number is even, add  
+# it to the `even_numbers` array.
+even_numbers = []
+for number in numbers:
+    if number%2 == 0:
+        even_numbers.append(number)
+print(even_numbers)
 
 # Problem 10
 # Do not edit the code below.
 score = 74
 # Do not edit the code above.
 
-# Determine if the letter grade of the given variable 'score'. If the variable is a 90 or above, console-log an 'A', between 80 and 89, console-log a 'B', between 70 and 79, 'C', between 60 and 69, 'D', and anything below 60 should console-log an 'F'.
+# Determine if the letter grade of the given variable 'score'. If the variable is a 90 or 
+# above, console-log an 'A', between 80 and 89, console-log a 'B', between 70 and 79, 'C', 
+# between 60 and 69, 'D', and anything below 60 should console-log an 'F'.
+if score >= 90:
+    print("A")
+elif score <90 and score >=80:
+    print("B")
+elif score <80 and score >=70:
+    print("C")
+elif score <70 and score >=60:
+    print("D")
+elif score <60:
+    print("F")
 
 
 # Problem 11
 # Create a variable called 'changeMyMind' and set it equal to true. 
-# Check to see if changeMyMind is set to true or false, if it is true, change the status to false, if it is false, change the status to true.
+# Check to see if changeMyMind is set to true or false, if it is true, change the status 
+# to false, if it is false, change the status to true.
+changeMyMind = True
 
+if changeMyMind:
+    changeMyMind = not changeMyMind
+else:
+    changeMyMind = True
 
+print(changeMyMind)
 # ADVANCED
 
 # For problems 12-15, use the following line of code:
 friends = ['Joe', 'Sally', 'Camilo', 'Perry', 'Susan']
 
 # Problem 12
-# Research to find the Python method that allows you to add an element to the end of the array (similar to push() in JavaScript), then add a name to the end of the `friends` array.
+# Research to find the Python method that allows you to add an element to the end of the 
+# array (similar to push() in JavaScript), then add a name to the end of the `friends` array.
+friends.append('Brunhilde')
 
 
 # Problem 13
-# Print out the total amount of elements in the `friends` array. The Python method you are looking for is similar to the JavaScript property `.length`.
-
+# Print out the total amount of elements in the `friends` array. The Python method you 
+# are looking for is similar to the JavaScript property `.length`.
+print(len(friends))
 
 # Problem 14
-# Add a name into the third position in the array (index 2). Make sure you are not overwriting the value that is already there.
-
-
+# Add a name into the third position in the array (index 2). Make sure you are not 
+# overwriting the value that is already there.
+friends.insert(2, "Plagueis the Wise")
 # Problem 15
-# Remove the last item in the array (try to think about how you can do this dynamically, meaning, if the array contents were to change, your code would still work).
+# Remove the last item in the array (try to think about how you can do this dynamically, 
+# meaning, if the array contents were to change, your code would still work).
+friends.pop()
+
+print(friends)
